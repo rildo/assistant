b5f20dadc9244e7c7876ad78124ec8284a7265e0
rildo <rildoxx@hotmail.fr>
2015-03-20 23:11:32 +0100

    Ajout de script par répertoire

--- app/Controller/ScriptsController.php
+++ app/Controller/ScriptsController.php
@@ -45,37 +45,56 @@ class ScriptsController extends AppController {
 	 * Method to add scripts
 	 *
 	 * Steps :
-	 * 	 1-
+	 * 	 1- Insert script or directory path
+	 *   2- Select one or many scripts
+	 *   3- Add them
+	 *
+	 *   /home/rildo/Documents/scripts/
 	 *
 	 * @return void
 	 */
 	public function add() {
-		$this->set('title_for_layout', 'Scripts - ajouter');
 		$step = 1;
+		$scripts = array();
+		$this->set('title_for_layout', 'Scripts - ajouter');
+		if (!empty($this->request->data) && array_key_exists('scripts', $this->request->data) && array_key_exists('step', $this->request->data['scripts'])) {
+			$step = $this->request->data['scripts']['step'];
+		} else {
+			$step = 1;
+		}
 		if ($this->request->is('post') & $step==1) {
 			// check of the first step form
 			// input is the scripts
 			if (is_file($this->request->data['script'])) {
-				$this->set('step', 3);
+				$step = 3;
 			// input is the directory
 			} elseif (is_dir($this->request->data['script'])) {
 				// Passage à l'étape de sélection des scripts à importer
-				$this->set('step', 2);
-				$scripts = glob($this->request->data['script'].'/*.*');
-				$this->set(compact('scripts'));
+				$step = 2;
+				$globSearch = $this->request->data['script'].'/*.*';
+				$globSearch = str_replace('//','/',$globSearch);
+				$scripts = glob($globSearch);
 			} else {
 				$this->Session->setFlash(__('La valeur saisie ne correspond ni à un script, ni à un répertoire existant.'),'notif', array('type' => 'danger'));
 			}
-// 			$this->Script->create();
-// 			if ($this->Script->save($this->request->data)) {
-// 				$this->Session->setFlash(__('The script has been saved.'));
-// 				return $this->redirect(array('action' => 'index'));
-// 			} else {
-// 				$this->Session->setFlash(__('The script could not be saved. Please, try again.'));
-// 			}
+		} elseif($this->request->is('post') & $step==2) {
+			foreach ($this->request->data('scripts') as $key => $value) {
+				if (preg_match('#^(checked_scripts)#', $key)) {
+					$scriptToAdd[] = $this->compose_script(base64_decode($value));
+				}
+			}
+			$this->Script->create();
+			if ($this->Script->saveMany($scriptToAdd)) {
+				$this->Session->setFlash(__('Les scripts ont bien été ajoutés.'),'notif', array('type' => 'success'));
+				return $this->redirect(array('action' => 'index'));
+			} else {
+				$this->Session->setFlash(__('Les scripts n\'ont pas pu être ajoutés, veuillez réessayer unitairement.'),'notif', array('type' => 'danger'));
+			}
 		} else {
-			$this->set('step', 1);
+			$step = 1;
 		}
+		$this->set(compact('step'));
+		$this->set(compact('scripts'));
 	}
 
 /**
@@ -116,10 +135,18 @@ class ScriptsController extends AppController {
 		}
 		$this->request->allowMethod('post', 'delete');
 		if ($this->Script->delete()) {
-			$this->Session->setFlash(__('The script has been deleted.'));
+			$this->Session->setFlash(__('Le script a bien été supprimé.'),'notif', array('type' => 'success'));
 		} else {
-			$this->Session->setFlash(__('The script could not be deleted. Please, try again.'));
+			$this->Session->setFlash(__('Une erreur est survenue, le script n\'a pas été supprimé.'),'notif', array('type' => 'danger'));
 		}
 		return $this->redirect(array('action' => 'index'));
+	}
+	
+	private function compose_script ($script) {
+		$infos = pathinfo($script);
+		return  $script = array(
+			'name' 				=> basename($script, '.'.$infos['extension']),
+			'script_location' 	=> $script,
+		);
 	}
 }
--- app/View/Scripts/add.ctp
+++ app/View/Scripts/add.ctp
@@ -1,13 +1,13 @@
-<div class="users index">
+<div class="scripts add">
 	<h2>
 		<?php echo __('Ajouter un/des script(s)'); ?>
 		<div class="action-space">
-			<?php echo $this->Html->link('Ajouter un script',array('controller' => 'scripts', 'action' => 'index'),array('class' => 'button')); ?>
+			<?php echo $this->Html->link('Liste des scripts', array('controller' => 'scripts', 'action' => 'index'),array('class' => 'button')); ?>
 		</div>
 	</h2>
 	<?php
 		if (empty($step) || $step == 1):
-			echo $this->Form->create('User',array('class' => 'row'));
+			echo $this->Form->create('Script',array('class' => 'row'));
 	?>
 		<div class="sub-content row">
 			<div class="columns">
@@ -28,7 +28,37 @@
 		</div>
 	<?php
 			echo $this->Form->end();
-		elseif(true):
+		elseif(!empty($step) || $step == 2):
+			echo $this->Form->create('scripts',array('class' => 'row'));
 	?>
-	<?php endif; ?>
+			<div class="sub-content row">
+				<table class="u-full-width">
+					<thead>
+						<tr>
+							<th><?php echo __('#'); ?></th>
+							<th><?php echo __('Script'); ?></th>
+						</tr>
+					</thead>
+					<tbody>
+						<?php foreach ($scripts as $key => $script): ?>
+							<tr>
+								<td><?php echo $this->Form->checkbox('checked_scripts'.'_'.$key,array('hiddenField' => false,'value'=>base64_encode($script))); ?></td>
+								<td><?php echo __($script); ?></td>
+							</tr>
+						<?php endforeach; ?>
+					</tbody>
+				</table>
+				<?php
+					echo $this->Form->input('step',array('value'=>2,'type'=>'hidden'));
+					echo $this->Form->button(
+						'Ajouter',
+						array(
+							'class' => 'button-primary'
+					));
+				?>
+			</div>
+	<?php
+			echo $this->Form->end();
+		endif;
+	?>
 </div>
